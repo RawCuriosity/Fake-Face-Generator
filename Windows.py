a8907b37c3c7249647090e79af8174d026268d7e
RawCuriosity <91442110+RawCuriosity@users.noreply.github.com>
2022-05-14 23:30:13 -0400

    Create Windows.py

---
+++ Windows.py
@@ -0,0 +1,16 @@
+import requests
+import time
+
+x = 1
+url = 'https://www.thispersondoesnotexist.com/image'
+
+number = int(input("How many faces would you like to generate?\n"))
+
+while (x - 1) < number:
+  response = requests.get(url)
+  file = open("images\\image" + str(x) + ".png", "wb")
+  file.write(response.content)
+  file.close()
+  print ("Created " + str(file) + "!")
+  x = x + 1
+  time.sleep(0.75)
